f95105312d0c5be337d0dab31fa1cf477566fdd2
Yixian <puyixian@gmail.com>
2020-07-10 23:38:36 +0800

    set up build env

---
+++ webpack.dev.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTSCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/App.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  resolve: {
+    extensions: [
+      '.js', '.jsx', '.ts', '.tsx'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.cm\.styl$/,
+        loader: 'style-loader!css-loader?modules&camelCase&localIdentName=[local]-[hash:base64:5]!stylus-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'assets/index.html'
+    }),
+    new ForkTSCheckerWebpackPlugin()
+  ],
+};
---
+++ webpack.prod.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTSCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/Table/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: [
+      '.js', '.jsx', '.ts', '.tsx'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.cm\.styl$/,
+        loader: 'style-loader!css-loader?modules&camelCase&localIdentName=[local]-[hash:base64:5]!stylus-loader'
+      }
+    ]
+  },
+  plugins: [
+    new ForkTSCheckerWebpackPlugin(),
+  ],
+};
